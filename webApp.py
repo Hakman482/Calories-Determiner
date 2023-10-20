bd4d64e9c75112bffa8ecd6070fd474556e7f2f5
Hakman482 <128440473+Hakman482@users.noreply.github.com>
2023-10-20 16:28:00 +0000

    Update webApp.py
    changed location of trained_model

--- webApp.py
+++ webApp.py
@@ -11,7 +11,7 @@ import streamlit as st
 import math
 
 # loading the saved model
-loaded_model = pickle.load(open("C:/Users/user/Desktop/ML/trained_model.sav","rb"))
+loaded_model = pickle.load(open("trained_model.sav","rb"))
 
 
 #Creating a function for prediction
